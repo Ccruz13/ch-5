901a486c0a3e459496623f4376b6de2601e714b3
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2021-10-15 14:25:27 -0400

    added savebtn function and local storage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// display current day
+// Display current day
 var todayDate = moment().format('ddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
@@ -10,7 +10,6 @@ $(document).ready(function() {
 
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-            console.log(blockTime, currentHour)
 
             if (blockTime < currentHour) {
                 $(this).addClass("past");
@@ -28,7 +27,27 @@ $(document).ready(function() {
         })
     };
 
-    
+
+    //Click listener for save button
+    $(".saveBtn").on("click", function () {
+
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        //Saves text and time to local storage
+        localStorage.setItem(time, text);
+    })
+
+    //Local storage
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     time();
-})
+});
